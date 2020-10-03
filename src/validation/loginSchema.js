1744338c0035ce7bcef7ff4468d80f68a048411f
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-10-03 13:44:07 +0300

    update validation schema

--- src/validation/loginSchema.js
+++ src/validation/loginSchema.js
@@ -1,12 +1,12 @@
 import * as yup from "yup";
 import errorMessages from "./errorMessages";
-const emailRegExp = /^([a-zA-Z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+// const emailRegExp = /^([a-zA-Z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
 
 const loginSchema = yup.object({
   email: yup
     .string()
     .required(errorMessages("Email"))
-    .matches(emailRegExp, errorMessages("Email", "Invalid")),
+    .email(errorMessages("Email", "Invalid")),
   password: yup.string().required(errorMessages("Password")).min(5).max(24),
 });
 
--- src/validation/registrationSchema.js
+++ src/validation/registrationSchema.js
@@ -1,16 +1,18 @@
 import * as yup from "yup";
 import errorMessages from "./errorMessages";
-const emailRegExp = /^([a-zA-Z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+// const emailRegExp = /^([a-zA-Z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
 
 const registrationSchema = yup.object({
   email: yup
     .string()
     .required(errorMessages("Email"))
-    .matches(emailRegExp, errorMessages("Email", "Invalid")),
-  password: yup.string().required(errorMessages("Password")).min(8).max(24),
-  full_name: yup.string().required(errorMessages("Full Name"))
-  .min(3, "Your full name must contain at least 3 charecters")
-  .max(40, "Your full name cannot contain more than 40 charecters"),
+    .email(errorMessages("Email", "Invalid")),
+  password: yup.string().required(errorMessages("Password")).min(5).max(24),
+  full_name: yup
+    .string()
+    .required(errorMessages("Full Name"))
+    .min(3, "Your full name must contain at least 3 charecters")
+    .max(40, "Your full name cannot contain more than 40 charecters"),
 });
 
 export default registrationSchema;
