a0de0d241856ecac4529f4bd7b29fa5bcab1b9d6
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-10-13 17:22:41 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { httpRequest } from "./api";
 import { Registration, Login } from "./screens/auth";
-
+import axios from 'axios'
 const App = () => {
   const [login, setLogin] = useState(true);
 
   const indexReq = async () => {
     try {
-      const data = await httpRequest.get("/");
+      const data = await axios.get('https://api.themoviedb.org/3/trending/all/week?api_key=0ec7db52277d0e30290bb2883e939895');
+      // const data = await httpRequest.get("/");
       console.log(data);
     } catch (err) {
       console.log("catch");
       console.log(err);
     }
+   
   };
+
+  useEffect(()=>{
+    const fetchData = async () => {
+       const data = await axios.get('https://api.themoviedb.org/3/trending/all/week?api_key=0ec7db52277d0e30290bb2883e939895');
+      // const data = await httpRequest.get("/");
+      console.log(data);
+    }
+    fetchData()
+  })
+
   return (
     <div className="App">
       {login ? <Login /> : <Registration />}
