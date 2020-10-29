4404a2584b62a890ebab1b0e2393aacb93eeeab7
tbookra <tbookra@gmail.com>
2020-10-29 12:06:18 +0200

    search bar added
    no styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,56 @@
 import React from 'react';
-import {AppBar, Toolbar, Typography, makeStyles} from '@material-ui/core'
+import {AppBar, Toolbar, Typography, makeStyles,fade, InputBase } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
 import { useSelector} from 'react-redux';
 import Drawer from './header/Drawer'
 import {Link} from 'react-router-dom'
-  const useStyles = makeStyles({
+  const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   title: {
     flexGrow: 1,
   },
-});
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 
 const Navbar = (props) => {
@@ -32,6 +72,21 @@ const Navbar = (props) => {
               Netflix
             </Typography>
           </Link>
+
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </div>
+
             <div>
               <Drawer/>
             </div>
