5aa600ad06b2bb0603d2bdc75d43c8cde1673147
tbookra <tbookra@gmail.com>
2020-10-29 15:39:28 +0200

    search page added
    no functionality

--- src/screens/app/SearchPage.jsx
+++ src/screens/app/SearchPage.jsx
@@ -1,12 +1,91 @@
 import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import SearchIcon from '@material-ui/icons/Search';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 
-const SearchPage = () => {
-return (
-    <div>
-        search
-    </div>
-)
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}));
+
+ const SearchPage = () => {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <SearchIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Search your favorate show here
+        </Typography>
+        <form className={classes.form} noValidate>
+       
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="search"
+            label='Search Here...'
+            name="search"
+            autoComplete="search"
+            autoFocus
+            
+          />
+           
+         
+          
+          <Button
+            type="submit"
+            
+            variant="contained"
+            color="secondary"
+            className={classes.submit}
+          >
+              
+              Search 
+            <SearchIcon />
+          </Button>
+          
+        </form>
+      </div>
+    
+    </Container>
+  );
 };
 
 export default SearchPage;
