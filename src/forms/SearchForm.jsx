08851da3bafc393479c84d6f64e38d8d1ab76aa5
tbookra <tbookra@gmail.com>
2020-11-05 09:55:52 +0200

    fetching the data by string
    getting the arrey of 20 objects from tmdb

--- src/forms/SearchForm.jsx
+++ src/forms/SearchForm.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Formik } from "formik";
 import {searchSchema} from '../validation';
+import {getSearchMovies} from '../config/movies-config';
+import tmdb from '../api/tmdb'
 import Input from '../components/Input';
 
  const SearchForm = (props) => {
+
   return (
         <Formik
         initialValues={{
           searchString: "", 
         }}
-        onSubmit={(values)=>console.log(values)}
+        onSubmit={ async (values)=>{
+          // console.log('values',values.searchString)
+          const url = getSearchMovies(values.searchString);
+          // console.log('url',url)
+          const {data:{results}} = await tmdb.get(url);
+          console.log(results)
+        }}
         // validationSchema={searchSchema}
       >
         {(props) => (
