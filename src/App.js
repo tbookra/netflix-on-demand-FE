4315ef540b129cddaed9c6e66cb40f4f684adfdc
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-11-09 13:51:26 +0200

    create darkmode hook and extracting theme from index.js to app.js

--- src/App.js
+++ src/App.js
@@ -2,35 +2,40 @@ import React from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import { routes } from './config/routes-config';
-import {Navbar,PrivateRoute} from './components';
+import { Navbar, PrivateRoute } from './components';
+import { ThemeProvider, createMuiTheme, CssBaseline } from '@material-ui/core'
 
+{/* <ThemeProvider theme={theme}>
+  <CssBaseline />
+  <App />
+</ThemeProvider> */}
 const App = () => {
 
   return (
     <div className="App ui container">
 
       <Navbar />
-            <Switch>
-           {
-             routes.map((route,index)=>(
-               route.privateRoute?
-               <PrivateRoute
-               key={index}
-               path={route.path}
-               component={route.component}
-               exact={route.exact}
-               />
-               :
-                <Route
-               key={index}
-               path={route.path}
-               component={route.component}
-               exact={route.exact}
-               />
+      <Switch>
+        {
+          routes.map((route, index) => (
+            route.privateRoute ?
+              <PrivateRoute
+                key={index}
+                path={route.path}
+                component={route.component}
+                exact={route.exact}
+              />
+              :
+              <Route
+                key={index}
+                path={route.path}
+                component={route.component}
+                exact={route.exact}
+              />
 
-             ))
-           }
-            </Switch>
+          ))
+        }
+      </Switch>
 
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,30 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider as StoreProvider } from "react-redux";
-import {PersistGate} from 'redux-persist/es/integration/react'
-import {persistedStore, store} from "./store/configureStore";
-import {ThemeProvider, createMuiTheme, CssBaseline} from '@material-ui/core'
+import { PersistGate } from 'redux-persist/es/integration/react'
+import { persistedStore, store } from "./store/configureStore";
 
-// const theme = (darkMode) => createMuiTheme({
-//   palette:{
-//     type:darkMode?"dark":"light"
-//   }
-// })
 
-console.log('index store', store.getState())
 ReactDOM.render(
   <React.StrictMode>
     <StoreProvider store={store}>
       <PersistGate persistor={persistedStore} loading={null}>
         <BrowserRouter>
-          <ThemeProvider theme={theme}>
-            <CssBaseline/>
-            <App />
-          </ThemeProvider>
+          <App />
         </BrowserRouter>
       </PersistGate>
     </StoreProvider>
