16bdbadee8bdf685fe9e9c38cc4a11e165b44224
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-11-21 15:46:51 +0200

    adding a membership api call

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -68,7 +68,7 @@ const tiers = [
   },
 ];
 
-const Pricing = ({ addMovie }) => {
+const Pricing = ({ addMovie, buyMembership }) => {
   const { currentMovie } = useSelector((state) => state.mainApp);
   const classes = useStyles();
 
@@ -148,7 +148,10 @@ const Pricing = ({ addMovie }) => {
                     fullWidth
                     variant={tier.buttonVariant}
                     color="primary"
-                    onClick={() => addMovie(currentMovie.id)}
+                    onClick={() => {
+                      if (tier.title === "Buy Movie") addMovie(currentMovie.id);
+                      else buyMembership();
+                    }}
                   >
                     {tier.buttonText}
                   </Button>
