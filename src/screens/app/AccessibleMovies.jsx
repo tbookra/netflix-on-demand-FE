20c0ae2c30f7a71d1e04f32bd09fb5229243768a
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-12-22 16:31:22 +0200

    commit

--- src/screens/app/AccessibleMovies.jsx
+++ src/screens/app/AccessibleMovies.jsx
@@ -1,14 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { httpRequest } from "../../api";
 import { useHistory, Link } from "react-router-dom";
-import {
-  Typography,
-  Container,
-  Button,
-  Grid,
-  CircularProgress,
-} from "@material-ui/core";
-import { AccessibleMoviesGrid } from "../../components";
+import { Typography, Container, Button, Grid } from "@material-ui/core";
+import { AccessibleMoviesGrid, Indicator } from "../../components";
 
 const AccessibleMovies = () => {
   const history = useHistory();
@@ -24,9 +18,6 @@ const AccessibleMovies = () => {
         if (data.isMember) return setIsMember(data.isMember);
         setAccessibleMovies(data.accessibleMovies);
         setIsLoading(false);
-        const { data } = await httpRequest.get("/movie/getAccessibleMovies");
-        if (data.isMember) return setIsMember(data.isMember);
-        setAccessibleMovies(data.accessibleMovies);
       } catch (err) {
         console.log(err);
       }
@@ -45,14 +36,7 @@ const AccessibleMovies = () => {
     }
   };
 
-
-  if (isLoading) {
-    return (
-      <Container>
-        <CircularProgress color="secondary" size={100} />
-      </Container>
-    );
-  }
+  if (isLoading) return <Indicator />;
 
   if (isMember && !isLoading) {
     return (
@@ -67,7 +51,6 @@ const AccessibleMovies = () => {
       </Container>
     );
   }
-
 
   if (!accessibleMovies.length && !isLoading) {
     return (
