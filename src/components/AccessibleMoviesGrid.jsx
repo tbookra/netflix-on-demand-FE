a2fa07a048e3846d1303501e538e72cb732efc61
aviram5 <60235058+aviram5@users.noreply.github.com>
2021-01-15 17:22:56 +0200

    Update AccessibleMoviesGrid.jsx

--- src/components/AccessibleMoviesGrid.jsx
+++ src/components/AccessibleMoviesGrid.jsx
@@ -4,25 +4,67 @@ import { Link } from "react-router-dom";
 import Skeleton from "@material-ui/lab/Skeleton";
 import { insertMovie } from "../actions/appActions";
 import { useDispatch } from "react-redux";
-import { Grid } from "@material-ui/core";
+import { Button, Container, Grid } from "@material-ui/core";
 import PropTypes from "prop-types";
-const AccessibleMoviesGrid = ({ accessibleMovies }) => {
+
+const AccessibleMoviesGrid = ({ accessibleMovies, handlePagination }) => {
   const dispatch = useDispatch();
 
-  return accessibleMovies.map((movie, index) =>
-    movie ? (
-      <Grid item xs={12} sm={4} md={3} key={index} className="moviesRowItem">
-        <Link
-          to={`/movieItem/${movie.movieId}`}
-          onClick={() => dispatch(insertMovie(movie.movieId))}
+  return (
+    <Container>
+      <Container style={{ margin: 20 }}>
+        <Button
+          style={{ marginRight: 10, marginLeft: 10 }}
+          onClick={() => handlePagination("down")}
         >
-          <img src={getMovieImgByPath(movie.posterPath)} alt="img" />
-        </Link>
+          Prev Page
+        </Button>
+        <Button
+          style={{ marginRight: 10, marginLeft: 10 }}
+          onClick={() => handlePagination("up")}
+        >
+          Next Page
+        </Button>
+      </Container>
+      <Grid container>
+        {accessibleMovies.map((movie, index) =>
+          movie ? (
+            <Grid
+              item
+              xs={12}
+              sm={4}
+              md={3}
+              key={index}
+              className="moviesRowItem"
+            >
+              <Link
+                to={`/movieItem/${movie.movieId}`}
+                onClick={() => dispatch(insertMovie(movie.movieId))}
+              >
+                <img src={getMovieImgByPath(movie.posterPath)} alt="img" />
+              </Link>
+            </Grid>
+          ) : (
+            <Skeleton variant="rect" width={210} height={118} />
+          )
+        )}
       </Grid>
-    ) : (
-      <Skeleton variant="rect" width={210} height={118} />
-    )
+    </Container>
   );
+  // return accessibleMovies.map((movie, index) =>
+  //   movie ? (
+  //     <Grid item xs={12} sm={4} md={3} key={index} className="moviesRowItem">
+  //       <Link
+  //         to={`/movieItem/${movie.movieId}`}
+  //         onClick={() => dispatch(insertMovie(movie.movieId))}
+  //       >
+  //         <img src={getMovieImgByPath(movie.posterPath)} alt="img" />
+  //       </Link>
+  //     </Grid>
+  //   ) : (
+  //     <Skeleton variant="rect" width={210} height={118} />
+  //   )
+  // );
 };
 
 AccessibleMoviesGrid.propTypes = {
